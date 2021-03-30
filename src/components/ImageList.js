9fbb17176ce5666df901ae3b59bf67659f1e3259
Alexey432 <66836963+alexey432@users.noreply.github.com>
2021-03-30 12:50:13 +0300

    Update ImageList.js

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -8,8 +8,8 @@ import ImageCard from './ImageCard';
 const ImageList = (props) => {
     
     const images = props.images.map((image) => {
-        return <ImageCard key={image.id} image={image}/>; //key only to root element!
-    })
+        return <ImageCard key={image.id} image={image}/>;
+    });
     
     return (
         <div className="image-list">{images}</div>
